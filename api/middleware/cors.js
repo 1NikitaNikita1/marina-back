9d6562c15fc1bdb15c8531b4d06332a2190bd3e4
qeqeqe123 <nikita@claimr.io>
2025-07-29 00:38:40 +0300

    test

--- api/middleware/cors.js
+++ api/middleware/cors.js
@@ -1,7 +1,7 @@
 // api/middleware/cors.js
 module.exports = (req, res, next) => {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.marynastrategy.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
